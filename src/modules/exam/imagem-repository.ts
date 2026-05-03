a260ca6871b39f92aa70fdaf1ec030ea0c1a02c8
Arthur Ribeiro <artrsousa1@gmail.com>
2026-05-03 16:34:53 -0300

    feat: add entity and repository for exam images
    Co-authored-by: Eric Camargo Silva <eric.camargo.silva@gmail.com>

---
+++ src/modules/exam/imagem-repository.ts
@@ -0,0 +1,10 @@
+import type { Imagem } from './imagem';
+
+export type FindImagensInput = {
+  examId?: string;
+};
+
+export type ImagemRepository = {
+  findMany(input: FindImagensInput): Promise<Imagem[]>;
+  createMany(imagens: Imagem[]): Promise<Imagem[]>;
+};
