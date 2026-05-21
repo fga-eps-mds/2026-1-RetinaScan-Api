c89e96b1e5b0d4a37f1348e4e5e5c747fb3f35dd
Arthur Ribeiro <artrsousa1@gmail.com>
2026-05-21 19:04:19 -0300

    feat: implement DrizzleExamIaErrorRepository

--- src/infra/database/drizzle/repositories/index.ts
+++ src/infra/database/drizzle/repositories/index.ts
@@ -3,3 +3,4 @@ export * from './drizzle-solicitacao-repository';
 export * from './drizzle-exame-repository';
 export * from './drizzle-imagem-repository';
 export * from './drizzle-resultado-ia-repository';
+export * from './drizzle-exam-ia-error-repository';
